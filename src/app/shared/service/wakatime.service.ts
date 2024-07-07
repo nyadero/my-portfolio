bba75fb3e0eb8598f4df03a394c58587486b4c56
nyadero <briannyadero443@gmail.com>
2024-07-07 15:01:15 +0300

    removed wakatime secret key

--- src/app/shared/service/wakatime.service.ts
+++ src/app/shared/service/wakatime.service.ts
@@ -8,16 +8,15 @@ import { Observable } from 'rxjs';
 export class WakatimeService {
 
   private apiUrl = 'https://api.wakatime.com/api/v1/';
-  private apiKey = 'waka_4fb728fa-e779-4c55-ab5a-4580d3ea1e91'; 
 
   constructor(private http: HttpClient) { }
 
   getWakatimeData(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': `Basic ${btoa(this.apiKey)}` // Encode the API key in base64
-    });
+    // const headers = new HttpHeaders({
+    //   'Authorization': `Basic ${btoa(this.apiKey)}` // Encode the API key in base64
+    // });
 
-    return this.http.get(`${this.apiUrl}/users/current/stats/last_7_days`, { headers });
+    return this.http.get(`${this.apiUrl}/users/current/stats/last_7_days`);
   }
 
 }
